10cd098737fc6eb0c1a363cec9daec2c9356ceb5
WilliamTuominiemi <william.m.tuominiemi@gmail.com>
2022-10-05 13:25:23 +0300

    texture on object from server

--- server/server.js
+++ server/server.js
@@ -6,6 +6,15 @@ const fs = require('fs')
 const path = require('path')
 const dirPath = path.join(__dirname, '/texture.png')
 
+const cors = require('cors')
+const corsOptions = {
+    origin: '*',
+    credentials: true, //access-control-allow-credentials:true
+    optionSuccessStatus: 200,
+}
+
+app.use(cors(corsOptions)) // Use this after the variable declaration
+
 app.get('/', (req, res) => {
     console.log(__dirname + '/texture.png')
     res.sendFile(dirPath)
