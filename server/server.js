4f294e312129eca8a84dd5d5d70ef5dc9bbbb1c7
WilliamTuominiemi <william.m.tuominiemi@gmail.com>
2022-10-11 15:33:32 +0300

    socket.io server

--- server/server.js
+++ server/server.js
@@ -1,24 +1,9 @@
-const express = require('express')
-const app = express()
-const port = 8080
-
-const fs = require('fs')
-const path = require('path')
-const dirPath = path.join(__dirname, '/texture1.png')
-
-const cors = require('cors')
-const corsOptions = {
-    origin: '*',
-    credentials: true, //access-control-allow-credentials:true
-    optionSuccessStatus: 200,
-}
-
-app.use(cors(corsOptions)) // Use this after the variable declaration
-
-app.get('/', (req, res) => {
-    res.sendFile(dirPath)
+const io = require('socket.io')(8080, {
+    cors: {
+        origin: ['http://localhost:3000'],
+    },
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+io.on('connect', (socket) => {
+    io.emit('new-socket', socket.id)
 })
