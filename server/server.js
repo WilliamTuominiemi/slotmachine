444f8b56acb3c982fdfce883bb868a45e4343a90
williamtuominiemi <william.m.tuominiemi@gmail.com>
2022-10-13 00:31:43 +0300

    array of slots

--- server/server.js
+++ server/server.js
@@ -4,6 +4,15 @@ const io = require('socket.io')(8080, {
     },
 })
 
+const slots = [
+    {number: 0, name: 'Heart',      color: 'black'},
+    {number: 1, name: 'Northstar',  color: 'red'},
+    {number: 2, name: 'Lightning',  color: 'black'},
+    {number: 3, name: 'Davidsstar', color: 'red'},
+    {number: 4, name: 'Salmiak',    color: 'black'},
+    {number: 5, name: 'Piru',       color: 'red'},
+]
+
 io.on('connect', (socket) => {
     io.emit('new-socket', socket.id)
     socket.on('slot-rotation', (rotation) => {
